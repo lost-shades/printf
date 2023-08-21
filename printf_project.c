b0e887f45231776024853d5c8019d2d07be6b4d3
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-21 07:28:53 +0100

    corrected task 0

--- printf_project.c
+++ printf_project.c
@@ -95,7 +95,7 @@ int _printf(const char *format, ...)
 	int sentence;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL || (format[0] == '% ' && format[1] == '\0'))
 		return (-1);
 	sentence = handle_format(format, args);
 
