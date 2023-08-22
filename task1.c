c972db6d8c0395ae2b7e7d59e9be74e2c6cce8f6
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-22 22:33:36 +0100

    Update task1.c

--- task1.c
+++ task1.c
@@ -7,7 +7,7 @@
 * @args: argument holder
 * Return: characters printed
 */
-int print_num(va_list args)
+int print_num(va_list args) 
 {
 int chars_printed;
 int n;
@@ -15,24 +15,31 @@ unsigned int m, d, count;
 
 n = va_arg(args, int);
 chars_printed = 0;
-if (n < 0)
+
+if (n < 0) 
 {
 putka('-');
-m = n * -1;
+m = -n;
 }
 else
+{
 m = n;
+}
+
 d = m;
 count = 1;
+
 while (d > 9)
 {
 d /= 10;
 count *= 10;
 }
+
 for (; count >= 1; count /= 10)
 {
-putka(((m / count) % 10) + '0');
+putka((m / count) % 10 + '0');
 chars_printed++;
 }
+
 return (chars_printed);
 }
