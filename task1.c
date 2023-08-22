4c6d8dcbceff5ed133ed3d85af39fd7df33b912e
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-22 21:27:15 +0100

    Update and rename printf_project2.c to task1.c

---
+++ task1.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_num - prints an integer
+* @format: number of variable arguments
+* @args: argument holder
+* Return: characters printed
+*/
+int print_num(const char *format, va_list args)
+{
+int chars_printed = 0;
+int n = va_arg(args, int);
+unsigned int m, d, count;
+if (n < 0)
+{
+printer(45);
+m = n * -1;
+}
+else
+m = n;
+d = m;
+count = 1;
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+for (; count >= 1; count /= 10)
+{
+printer(((m / count) % 10) + 48);
+chars_printed++;
+}
+return (chars_printed);
+}
