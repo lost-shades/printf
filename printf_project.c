ea52e68df3a5b63d6741bdd5f26d4bd4736e381f
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-22 06:41:15 +0100

    task zero

--- printf_project.c
+++ printf_project.c
@@ -94,6 +94,8 @@ int _printf(const char *format, ...)
 	va_list args;
 	int sentence;
 
+	if (format[0] == '\0')
+		return (0);
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
