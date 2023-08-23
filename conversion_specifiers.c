78ae52519daf4792dfe2ff0a850fecc7a4aea966
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-23 05:40:44 +0100

    Create conversion_specifiers

---
+++ conversion_specifiers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* conversion_specifier - checks for conversion specifiers
+* @specifer: the characters to check
+* @args: list of argument.
+* Return: void
+*/
+
+void conversion_specifier(char specifier, va_list args) 
+{
+switch (specifier) 
+{
+case 'u':
+unsigned int num_u = va_arg(args, unsigned int);
+printf("%u", num_u);
+break;
+case 'o':
+unsigned int num_o = va_arg(args, unsigned int);
+printf("%o", num_o);
+break;
+case 'x':
+unsigned int num_x = va_arg(args, unsigned int);
+printf("%x", num_x);
+break;
+case 'X':
+unsigned int num_X = va_arg(args, unsigned int);
+printf("%X", num_X);
+break;
+}
+}
+
