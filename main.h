1dfd1117d4d9ea1e35105083fddcf7812fcd28f1
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-23 22:58:20 +0100

    Update main.h

--- main.h
+++ main.h
@@ -8,12 +8,5 @@ int _printf(const char *format, ...);
 int print_num(va_list args);
 int print_string(va_list args);
 int handle_format(const char *format, va_list args);
-int _strlen(char *s);
-char *rot13(char *s);
-void rev_string(char *s);
-void print_rev(char *s);
-void convert_to_binary(unsigned int num, char *binary, int length);
-void conversion_specifier(char specifier, va_list args);
-int print_hex(unsigned long num, int width);
 
 #endif
