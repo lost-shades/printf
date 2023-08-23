991f587d93baf7a12eed11d7ca1e2b33814e379f
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-23 03:47:00 +0100

    Update main.h

--- main.h
+++ main.h
@@ -8,5 +8,8 @@ int _printf(const char *format, ...);
 int print_num(va_list args);
 int print_string(va_list args);
 int handle_format(const char *format, va_list args);
+int _strlen(char *s);
+char *rot13(char *s);
+void print_rev(char *s);
 
 #endif
