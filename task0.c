d32c801f8ccc8f9a851d5ad8310106946b07aaa4
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-24 17:58:57 +0100

    Update task0.c

--- task0.c
+++ task0.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
-* putka - function to output a char
-* @c: character to be printed
-* Return: always 0
-*/
-int putka(char c)
-{
-write(1, &c, 1);
-return (0);
-}
-
-/**
 * print_string - function to print string
 * @args: pointer to string
 * Return: count if successful
@@ -82,26 +71,3 @@ count++;
 }
 return (count);
 }
-
-/**
-* _printf - printf string according to format specified
-* @format: The format string specifying the
-* types of arguments to be printed.
-* Return: the number of characters printed
-* (excluding the null byte used to end output to strings)
-*/
-int _printf(const char *format, ...)
-{
-va_list args;
-int sentence;
-
-va_start(args, format);
-sentence = 0;
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-sentence = handle_format(format, args);
-
-va_end(args);
-return (sentence);
-}
-
