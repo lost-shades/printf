d67f449f773659fae57d2f5cafe0ee46c8d2466a
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-24 18:00:53 +0100

    Update task0.c

--- task0.c
+++ task0.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
-* print_string - function to print string
-* @args: pointer to string
-* Return: count if successful
-*/
-int print_string(va_list args)
-{
-char *s;
-int count;
-
-count = 0;
-s = va_arg(args, char *);
-if (s == NULL)
-s = "(null)";
-while (*s != '\0')
-{
-putka(*s++);
-count++;
-}
-return (count);
-}
-
-/**
 * handle_format - function that handles format for _printf
 * @format: pointer to the characters to be printed
 * @args: variable name
