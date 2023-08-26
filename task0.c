d5e601932495afae50114ccfd44adf61134ff9a1
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-26 22:05:41 +0100

    Update task0.c

--- task0.c
+++ task0.c
@@ -1,6 +1,44 @@
 #include "main.h"
 
 /**
+* choose_specifier - function that chooses specifier
+* @c: a char
+* @args: argument list
+* Return: counter to chars
+*/
+
+int choose_specifier(char c, va_list args)
+{
+int counter;
+
+counter = 0;
+switch (c)
+{
+case 'c':
+putka(va_arg(args, int));
+counter++;
+break;
+case 's':
+counter += print_string(args);
+break;
+case '%':
+putka('%');
+counter++;
+break;
+case 'i':
+case 'd':
+counter += print_num(args);
+break;
+default:
+putka('%');
+putka(format[i]);
+counter += 2;
+break;
+}
+return (counter);
+}
+
+/**
 * handle_format - function that handles format for _printf
 * @format: pointer to the characters to be printed
 * @args: variable name
@@ -16,29 +54,7 @@ for (i = 0; format[i] && format[i] != '\0'; i++)
 if (format[i] == '%' && format[i + 1] != '\0')
 {
 i++;
-switch (format[i])
-{
-case 'c':
-putka(va_arg(args, int));
-count++;
-break;
-case 's':
-count += print_string(args);
-break;
-case '%':
-putka('%');
-count++;
-break;
-case 'i':
-case 'd':
-count += print_num(args);
-break;
-default:
-putka('%');
-putka(format[i]);
-count += 2;
-break;
-}
+count += choose_specifier(format[i], args);
 }
 else
 {
